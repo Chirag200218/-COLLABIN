19cc30abdf44ff0581a99702a0d83eeeac1795aa
TEMPTUN <parasharash6@gmail.com>
2023-05-21 17:44:44 +0530

    added file route

--- api/search/personCatSearch.js
+++ api/search/personCatSearch.js
@@ -1,5 +1,5 @@
-import Category from "../../../model/category";
-import connectmongo from "../../../utils/mongoconnect";
+import Category from "../../model/category";
+import connectmongo from "../../utils/mongoconnect";
 export default async function handler(req,res){
     await connectmongo();
     if(req.method==='GET'){
